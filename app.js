98d4d7612e82783e64d5dee5faee18a17be62ed7
SweetPicklee <32518710+SweetPicklee@users.noreply.github.com>
2019-02-20 02:38:48 +0000

    fixed blog buttons on chrome

--- app.js
+++ app.js
@@ -112,6 +112,6 @@ app.delete('/blogs/:id', function(req, res) {
 	//redirect somewhere
 });
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('SERVER IS RUNNING!');
 });
